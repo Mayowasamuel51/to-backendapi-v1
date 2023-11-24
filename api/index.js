22ce73c34ffd51286aa9d70de4fc992c57e584ff
mayowasamuel51 <fpasamuelmayowa51@gmail.com>
2023-11-24 12:24:32 +0000

    worked

--- api/index.js
+++ api/index.js
@@ -1,13 +1,44 @@
-// index.js 
-const express = require('express') 
-const app = express() 
-app.get('/api',(req,res)=>{ 
-	res.send(`<h5 style="color:green"> 
-		Hey Geek! you just deployed serverless express api</h5>`) 
-}) 
-app.listen(8080,()=>{ 
-	console.log('Server started at http://localhost:8080') 
-}) 
+const express = require('express');
+const app = express()
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+app.use(bodyParser.json()) // application/json
+const cors = require('cors')
+const dotenv = require('dotenv')
+const dotenvb = require('dotenv').config();
+const Middleware = require('../middleware/auth')
+mongoose.connect(process.env.DATABASE_URL).then((res)=>console.log('database connected!!!')).catch((err)=>console.log(err.message))
+
+app.use(cors())
+// // connecting the server and frontend
+app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Content-Allow-Orgin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH,DELTE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.send('okay!!!!!!!!!!!!!!!!!!')
+})
 
 
-module.exports=app
+// app.use('/api',testapi)
+
+
+/// genral error express
+app.use((error, req, res, next) => {
+    console.log(error.message)
+    const status = error.statusCode || 500
+    const message = error.message;
+    res.status(status).json({messsage:message})
+})
+
+
+const port = 3000 || process.env.PORT;
+app.listen(port, () => {
+    console.log('SERVER IS RUNNING')
+})
+
+module.exports = app;

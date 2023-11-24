8d25a339732c8bfc365aa3c0a74378ee65155058
mayowasamuel51 <fpasamuelmayowa51@gmail.com>
2023-11-24 05:52:04 +0000

    new add

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const { initializeApp } = require('firebase-admin/app');
 const cors = require('cors')
 const dotenv = require('dotenv')
 const dotenvb = require('dotenv').config();
-const Middleware = require('./middleware/auth')
+const Middleware = require('../middleware/auth')
 mongoose.connect(process.env.DATABASE_URL).then((res)=>console.log('database connected!!!')).catch((err)=>console.log(err.message))
 
 app.use(cors())

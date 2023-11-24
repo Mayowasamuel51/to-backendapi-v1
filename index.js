2a2698625cc12eb2a5be7e5db01f50548ce3ba24
mayowasamuel51 <fpasamuelmayowa51@gmail.com>
2023-11-24 11:09:46 +0000

    new commit

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const app = express()
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser')
-// app.use(bodyParser.json()) // application/json
-// const { initializeApp } = require('firebase-admin/app');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+app.use(bodyParser.json()) // application/json
+
 const cors = require('cors')
-// const dotenv = require('dotenv')
-// const dotenvb = require('dotenv').config();
+const dotenv = require('dotenv')
+const dotenvb = require('dotenv').config();
 const Middleware = require('./middleware/auth')
-// mongoose.connect(process.env.DATABASE_URL).then((res)=>console.log('database connected!!!')).catch((err)=>console.log(err.message))
+mongoose.connect(process.env.DATABASE_URL).then((res)=>console.log('database connected!!!')).catch((err)=>console.log(err.message))
 
 app.use(cors())
 // // connecting the server and frontend
-// app.use((req, res, next) => {
-//     res.setHeader('Content-Type', 'application/json');
-//     res.setHeader('Access-Content-Allow-Orgin', '*')
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH,DELTE')
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-//     next()
-// })
+app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Content-Allow-Orgin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH,DELTE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
 
 app.get('/', (req, res) => {
     res.send('okay!!!!!!!!!!!!!!!!!!')

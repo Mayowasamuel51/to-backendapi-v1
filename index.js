6b0616c80783f191210cd03e98f9951dd449a8f6
mayowasamuel51 <fpasamuelmayowa51@gmail.com>
2023-11-24 05:46:18 +0000

    firebae

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
-const express = require('express')
+const express = require('express');
 const app = express()
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+app.use(bodyParser.json()) // application/json
+const { initializeApp } = require('firebase-admin/app');
+const cors = require('cors')
 const dotenv = require('dotenv')
 const dotenvb = require('dotenv').config();
+const Middleware = require('./middleware/auth')
+mongoose.connect(process.env.DATABASE_URL).then((res)=>console.log('database connected!!!')).catch((err)=>console.log(err.message))
 
-app.get('/', (req, res, next) => {
-    res.send("working")
+app.use(cors())
+// connecting the server and frontend
+app.use((req, res, next) => {
+    res.setHeader('Access-Content-Allow-Orgin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH,DELTE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
+
+app.use(Middleware.decodeToken)
+
+app.get('/api', (req, res, next) => {
+    console.log(req.headers)
+   return  res.json({
+        post: [{
+            id:1,
+            name:"okay"
+        }]
+    })
 })
 
 
-app.get('/api/', (req, res, next) => {
-    res.status(200).json({name:"mayowa"})
+/// genral error express
+app.use((error, req, res, next) => {
+    console.log(error.message)
+    const status = error.statusCode || 500
+    const message = error.message;
+    res.status(status).json({messsage:message})
 })
 
 
-const port = 3000 || app.process.PORT;
-
-app.listen(port)
+const port = 3000 || process.env.PORT;
+app.listen(port, () => {
+    console.log('SERVER IS RUNNING')
+})
---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const admin = require('../firebase')
+
+class Middleware {
+    async decodeToken(req, res, next) {
+        const token = req.headers.authorization.split(' ')[1];
+        try {
+            const decodeValue =  await  admin.auth().verifyIdToken(token);
+            console.log(decodeValue)
+            if (decodeValue) {
+                return next()
+            }
+            return res.json({ message: "un Authorization by users" })
+        } catch (err) {
+            return res.json({ message:err.message })
+        }
+    }
+}
+
+module.exports = new Middleware();

de57d6b7737ac421d04e24e156d989ecf26359b5
mayowasamuel51 <fpasamuelmayowa51@gmail.com>
2023-11-26 13:45:26 +0000

    sda

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const app = require('./api/index')
+const cors = require('cors')
+
+app.use(cors())
+// // connecting the server and frontend
+app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Content-Allow-Orgin', 'https://www.to-analytics.com')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH,DELTE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
+
 module.exports = app

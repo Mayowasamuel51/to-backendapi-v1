5757a822336649c5893349ad4f55e2baa1d2f7e0
mayowasamuel51 <fpasamuelmayowa51@gmail.com>
2023-11-26 13:39:47 +0000

    sda

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ app.use(cors())
 // // connecting the server and frontend
 app.use((req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Content-Allow-Orgin', 'https://to-backendapi-v1.vercel.app')
+    res.setHeader('Access-Content-Allow-Orgin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH,DELTE')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     next()

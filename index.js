6f6f1aed543f49971eb777901945d313f8a6f250
mayowasamuel51 <fpasamuelmayowa51@gmail.com>
2023-12-25 13:49:27 +0100

    Sdaf

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 // // connecting the server and frontend
 app.use((req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Content-Allow-Orgin', '*')
+    res.setHeader('Access-Content-Allow-Orgin', 'https://www.to-analytics.com')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH,DELTE')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     response.head("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");

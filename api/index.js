d4489c18467538c242e18e2d29edb6f9df19b44c
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-11-07 10:27:26 +0100

    http://localhost:5173

--- api/index.js
+++ api/index.js
@@ -55,7 +55,7 @@ app.all("*", (req, res, next) => {
 // });
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.to-analytics.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.to-analytics.com', "http://localhost:5173");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   

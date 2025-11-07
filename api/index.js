ad032cd7ab1c68460606a4fbf18ee09c69f6ccd4
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-11-07 10:26:42 +0100

    gith

--- api/index.js
+++ api/index.js
@@ -55,13 +55,17 @@ app.all("*", (req, res, next) => {
 // });
 
 app.use((req, res, next) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', 'https://www.to-analytics.com');
-    // res.setHeader('Access-Content-Allow-Orgin', 'https://www.to-analytics.com')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH,DELTE')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    next()
-})
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.to-analytics.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  
+  next();
+});
 
 app.use("/api", pagesroutes);
 app.use("/api", dashboardroutes);
